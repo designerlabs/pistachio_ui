20095d4443678e426007e23ca136c528d6e98fdb
unknown <gomathi.masilamani@NL-5216.mimos.local>
2015-12-02 13:55:22 +0800

    ReportMgt loaded

--- main/js/controllers/ReportMgtController.js
+++ main/js/controllers/ReportMgtController.js
@@ -3,89 +3,159 @@
 MetronicApp.controller('ReportMgtController', function($rootScope, $scope, $http, $timeout) {
     $scope.$on('$viewContentLoaded', function() {   
 
-        $("#databaseList").show();
+        $("#reportMgtList").show();
         $("#tableList").hide();
 
         // initialize core components
         Metronic.initAjax();
 
 
-        var databases;
+        var reportMgts;
 
 
-        function databaseDataFunc() {
-            databases = $('#databaseData').DataTable({
+        function reportMgtDataFunc() {
+            reportMgts = $('#reportMgtdata').DataTable({
 
                 "ajax": {
                     "processing": true,
                     "serverSide": true,
-                    "url": globalURL+"etl/databases",
+                    "url": globalURL + "query/cato",
                     "dataSrc": ""
                 },
                 "columns": [{
-                        "data": "dbName"
+                		"data": "id"
+                    },{
+                        "data": "queryCategoryName"
                     }, {
-                        "data": "dbType"
+                        "data": "queryCategory"
                     }, {
-                        "data": "username"
+                        "data": "className"
                     }, {
-                        "data": "createdDate",
-                        "width": "10%"
+                        "data": "queryCategory"
+                    },{
+                        "data": "role"                        
+                    }, {
+                    	"data": "activated"
                     }, {
                         "data": "action",
-                        "width": "35%",
+                        "width": "20%",
                         "render": function(data, type, full, meta) {
-                            return '<button class="btn btn-success btn-sm tableView"><i class="fa fa-eye"></i> view</button><button class="btn btn-primary btn-sm updateBtn"><i class="fa fa-edit"></i> Edit</button><button class="btn btn-danger btn-sm deleteBtn"><i class="fa fa-trash"></i> Delete</button>';
+                            return '<button class="btn btn-primary btn-sm updateBtn"><i class="fa fa-edit"></i> Edit</button><button class="btn btn-danger btn-sm deleteBtn"><i class="fa fa-trash"></i> Delete</button>';
                         }
                     }
                 ]
             });
         }
         
-        formInputValidation("#databaseForm");
-        $("#databaseUISubmit").click(function(event) {
-            var databaseNameVal = $("#databaseForm #database-name").val();
-            var databaseTypeVal = $("#databaseForm #database-type").val();
-            var databaseURLVal = $("#databaseForm #database-url").val();
-            var databaseUserVal = $("#databaseForm #database-uname").val();
-            var databasePwdVal = $("#databaseForm #database-pwd").val();
-            var databaseDriverVal = $("#databaseForm #database-driver").val();
-            inputValidation("#databaseForm", databaseAjax);
+        formInputValidation("#reportMgtForm");
+        $("#reportMgtUISubmit").click(function(event) {
+            var reportMgtTitleVal = $("#reportMgtForm #reportMgt-title").val();
+            var reportMgtCategoryVal = $("#reportMgtForm #reportMgt-category").val();
+            var reportMgtThemeVal = $("#reportMgtForm #reportMgt-theme").val();
+            //var reportMgtIconVal = $("#reportMgtForm #reportMgt-icon").val();
+            var reportMgtRoleVal = $("#reportMgtForm #reportMgt-role").val();
+            var reportMgtActivateVal = $("#reportMgtForm #reportMgt-activate").val();
+            inputValidation("#reportMgtForm", reportMgtAjax);
 
-            function databaseAjax() {
+            function reportMgtAjax() {
                 $.ajax({
-                        url: globalURL+"etl/databases",
+                        url: globalURL+"query/cato",
                         type: "POST",
                         dataType: 'json',
                         contentType: "application/json; charset=utf-8",
                         data: JSON.stringify({
-                            dbName: databaseNameVal,
-                            dbType: databaseTypeVal,
-                            dbUrl: databaseURLVal,
-                            dbUsername: databaseUserVal,
-                            dbPassword: databasePwdVal,
-                            dbDriver: databaseDriverVal,
-                            active: "active",
-                            username: "admin"
-
+                            queryCategoryName: reportMgtTitleVal,
+                            queryCategory: reportMgtCategoryVal,
+                            className: reportMgtThemeVal,
+                            role: reportMgtRoleVal,
+                            activated: reportMgtActivateVal
                         })
                     })
                     .done(function() {
-                        databases.destroy();
-                        databaseDataFunc();
-                        $("#databaseAddForm").modal('hide');
-                        $("#databaseRequire").hide();
+                        reportMgts.destroy();
+                        reportMgtDataFunc();
+                        $("#reportMgtAddForm").modal('hide');
+                        $("#reportMgtRequire").hide();
                     })
                     .fail(function(data) {
                         console.log(data.responseJSON.error);
-                        $("#databaseRequire span").html(data.responseJSON.error);
-                        $("#databaseRequire").show();
+                        $("#reportMgtRequire span").html(data.responseJSON.error);
+                        $("#reportMgtRequire").show();
                         //alert('Failed!');
                     });
             }
         });
 
-        databaseDataFunc();
+        reportMgtDataFunc();
+        //Update Record
+        var selectedreportMgtId = undefined;
+        $('#reportMgtdata').on('click', 'button.updateBtn', function() {
+            $("#reportMgtForm input").parent('.form-group').removeClass('has-error');
+            var selectedreportMgt = reportMgts.row($(this).parents('tr')).data();
+            selectedreportMgtId = selectedreportMgt.id;
+            $("#reportMgtAddForm #reportMgtUIForm").addClass('hide');
+            $("#reportMgtAddForm #reportMgtUIUpdate").removeClass('hide');
+            $("#reportMgtForm #reportMgt-title").val(selectedreportMgt.reportMgtTitle);
+            $("#reportMgtForm #reportMgt-category").val(selectedreportMgt.reportMgtCategory);
+            $("#reportMgtForm #reportMgt-theme").val(selectedreportMgt.reportMgtTheme);
+            $("#reportMgtAddForm").modal('show');
+
+        });
+
+        $("#reportMgtUIUpdate").click(function(event) {
+            var reportMgtNameVal = $("#reportMgtForm #reportMgt-name").val();
+            var reportMgtVal = $("#reportMgtForm #reportMgt-value").val();
+            var reportMgtApp = $("#reportMgtForm #reportMgt-application").val();
+            //alert(selectedQueryId);
+            $.ajax({
+                    url: globalURL + "query/cato/",
+                    type: "PUT",
+                    dataType: 'json',
+                    contentType: "application/json; charset=utf-8",
+                    data: JSON.stringify({
+                        id: selectedreportMgtId,
+                        reportMgtName: reportMgtNameVal,
+                        reportMgtValue: reportMgtVal,
+                        application: reportMgtApp
+                    })
+                })
+                .done(function(data) {
+                    reportMgturation.destroy();
+                    reportMgturationDataFunc();
+                    $("#reportMgtAddForm").modal('hide');
+                    //selectedQueryId = null;
+                })
+                .fail(function() {
+                    alert('Failed!');
+                });
+        });
+        //Delete Record
+        var selectedreportMgtForDelete;
+        $('#reportMgtdata').on('click', 'button.deleteBtn', function() {
+            $("#reportMgtDelete").modal('show');
+            selectedreportMgtForDelete = reportMgts.row($(this).parents('tr')).data();
+            $("#reportMgtDelete .modal-body h3").html('Are you sure do you want to<br>delete the reportMgt <strong>' + selectedreportMgtForDelete.id + '</strong> ?');
+        });
+
+         $('#reportMgtDataDeleteBtn').click(function(event) {
+
+            $.ajax({
+                url: globalURL + 'query/cato/' + selectedreportMgtForDelete.id,
+                type: 'DELETE',
+                success: function(result) {
+                    // Do something with the result
+                    reportMgts.destroy();
+                    reportMgtsDataFunc();
+                    $("#reportMgtDelete").modal('hide');
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    $("#errorTitle").html(XMLHttpRequest.responseJSON.error);
+                    $("#reportMgtDeleteErrorMsg").modal('show');
+                    $("#reportMgtDelete").modal('hide');
+                }
+            });
+
+        });
 
 
         var getUser = localStorage.getItem("username");
