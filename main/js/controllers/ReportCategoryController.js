9381c3c267479b302dd1eb406af483af87344a1f
Sridhar <designerlabs@hotmail.com>
2016-01-22 17:04:07 +0800

    updated

--- main/js/controllers/ReportCategoryController.js
+++ main/js/controllers/ReportCategoryController.js
@@ -9,23 +9,45 @@ MetronicApp.controller('ReportCategoryController',  ['$rootScope', '$scope', '$h
         var getUser = localStorage.getItem("token");
         var getSideMenu = localStorage.getItem("sideMenuValue");
     	//$http.get(globalURL+"auth/token?token="+getUser)
-      $http.get(globalURL+'query/report/'+getSideMenu)
+      $http.get(globalURL+'auth/subreports?token='+getUser+'&parent='+getSideMenu)
     	.success(function(response) {
-    		$scope.names = response;
+
+
+            //$http({method: 'GET', url: 'json/json_price_1.json'}).success(function(data) {
+                $scope.names = [];
+                angular.forEach(response, function(value, key) {
+                    $scope.names.push(value);
+                });
+                $scope.isVisible = function(name){
+                    return true;// return false to hide this artist's albums
+                };
+            //});
+
+           /* $scope.names= response;
+            $scope.crudVal = [];
+            angular.forEach(response, function(value, key) {
+                $scope.crudVal.push(value.subReports);
+            });
+          
+            $scope.isVisible = function(name){
+                return true;// return false to hide this artist's albums
+            };*/
+            //console.log(response.subReports);
+    		//$scope.names = response;
+
        });
 
     	$scope.go = function(data){
-            
-            console.log(data);
-    		    location.href="#/reports/e-reporting.html";
-    		    categoryId = this.x.queryCategory;
-            categoryTitle = this.x.queryCategoryName;
+    		location.href="#/reports/e-reporting.html";
+            categoryId = this.x.subReports.queryCategory;
+            categoryTitle = this.x.subReports.queryCategoryName;
             $scope.category = categoryId;
             $scope.categoryTitle = categoryTitle;
             //$scope.message = sharedService.categoryId;
           	//$scope.myservice = categoryId; 
           	$("#reportCategoryID").val(categoryId);
           	//if(!localStorage.setItem("reportCategoryID"){
+            localStorage.setItem("reportPrivilege", this.x.add);
           	localStorage.setItem("reportCategoryID", categoryId);
             localStorage.setItem("reportCategoryTitle", categoryTitle);
           	//}	
