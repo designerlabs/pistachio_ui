1ca402c1b5bc83bf1561f9963583822bc691560d
Gomathi <gomsmaasi@gmail.com>
2016-02-17 15:12:55 +0800

    initial push myprofile

--- main/js/controllers/MyProfileController.js
+++ main/js/controllers/MyProfileController.js
@@ -1,25 +1,22 @@
 'use strict';
-
+var getToken = undefined;
 MetronicApp.controller('MyProfileController', function($rootScope, $scope, $http, $timeout) {
     $scope.noedit =true;
     $scope.$on('$viewContentLoaded', function() {
         Metronic.initAjax(); // initialize core components
 
- 	//	var getAuthority = localStorage.getItem("authorities");
+  //  var getAuthority = localStorage.getItem("authorities");
   //   alert(getAuthority);
- 		var getToken = localStorage.getItem("token");
+    getToken = localStorage.getItem("token");
   //   alert(getToken);
      $http.get(globalURL+'auth/user?token=' + getToken)
      .success(function(response) {
        $scope.firstName_1 = response.firstName;
        $scope.rank = response.rank;
        $scope.email = response.email;
-      // alert(response.firstName)
        $scope.setProfileInfo(response);
        $scope.noedit =true;
-      })
-
-      ;
+      });
 
     });
 
@@ -46,22 +43,34 @@ MetronicApp.controller('MyProfileController', function($rootScope, $scope, $http
 
     $scope.edit = function(){
       $scope.noedit = false;
+       $http.get(globalURL+'auth/user?token=' + getToken)
+      .success(function(response) {
+        $('.edit-form [name=editname]').val(response.firstName);    
+        $('.edit-form [name=editemail]').val(response.email);
+        $('.edit-form [name=editlang]').val((response.lang == "en") ? "English" : "Bahasa Malayu");
+       });
+
     };
     $scope.overview = function(){
-      $scope.noedit = true;
+       $scope.noedit =true;
+      $http.get(globalURL+'auth/user?token=' + getToken)
+     .success(function(response) {
+       $scope.setProfileInfo(response);      
+      });
     };
 
     $scope.setProfileInfo = function(info){
-      $('#myprofile-firstname').val(info.firstName);
-      $('#myprofile-lastname').val(info.lastName);
-      $('#myprofile-email').val(info.email);
-      $('#myprofile-unit').val(info.unit);
-      $('#myprofile-rank').val(info.rank);
-      $('#myprofile-state').val('not available');
-      $('#myprofile-branch').val(info.branch);
-      $('#myprofile-dept').val(info.department);
+      $('#myprofileDsp-firstname').val(info.firstName);
+      // $('#myprofileDsp-lastname').val(info.lastName);
+      $('#myprofileDsp-email').val(info.email);
+      $('#myprofileDsp-unit').val(info.unit);
+      $('#myprofileDsp-rank').val(info.rank);
+      // $('#myprofileDsp-state').val(info.state);
+      $('#myprofileDsp-branch').val(info.branch);
+      $('#myprofileDsp-dept').val(info.department);
     //  $('#myprofile-authority').val(getAuthority);
-        $('#myprofile-lang').val((info.lang == "en") ? "English" : "Bahasa Malayu");
+      var langval = (info.lang == "en") ? "English" : "Bahasa Malayu";
+      $('#myprofileDsp-lang').val(langval);
     };
 
     // set sidebar closed and body solid layout mode
