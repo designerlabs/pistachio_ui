48b86c2933bd42dbd5690ef09b4ae993abdcb008
Gomathi <gomsmaasi@gmail.com>
2016-02-25 17:15:12 +0800

    User workflow

--- main/js/controllers/WorkflowuserController.js
+++ main/js/controllers/WorkflowuserController.js
@@ -9,14 +9,20 @@ $scope.opt = 'submit';
         Metronic.initAjax(); // initialize core components
 	});
 	 var getUser = localStorage.getItem("username");
-		$http.get(globalURL+"workflow/request/")
+	 var getToken = localStorage.getItem("token");
+
+		$http.get(globalURL+"workflow/request?token=" + getToken)
 		.success(function(response) {
 			console.log(response);
 			$scope.names = response;
 	   });
-	 formInputValidation("#frmRequest");
+
+	 
+
 	 $scope.onClick = function (opt) {
-	 	// alert('Clicked');
+
+	 	formInputValidation("#frmRequest");
+	 	
 	 	var reportName = $('#userReqTitle').val();
 	 	var description = $('#userReqDes').val();
 	 	var priority = $('#userPriority option:selected').val();
@@ -34,7 +40,7 @@ $scope.opt = 'submit';
 				"adminEmail":"NA",
 				"createdDate":null,
 				"lastModifiedDate":null,
-				"user":"gomathi",
+				"user":getUser,
 				"reportName":reportName,
                 "priority": priority}),
           }).done(function (data) {
@@ -44,6 +50,34 @@ $scope.opt = 'submit';
               $('#userPriority option:selected').text("Please Select");
           }); 
       	}
+	 }
+
+	 $scope.viewReq = function(data){
+	 		$http.get(globalURL+"workflow/request/" + data.id)
+	 		.success(function(response) {
+	 			console.log(response);
+	 			$('#userReqTitle').val(response.reportName);
+	 			$('#userReqDes').val(response.description);
+	 			$('#userPriority option:selected').val(response.priority);
+	 	   });
+	 		$scope.reqid = data.id;
+	 		console.log($scope.reqid);
+	 }
+ 
+	 $scope.submitComment = function(){
+	 		 	 $.ajax({
+	 	              url: globalURL + "workflow/request/"+ $scope.reqid +"/comments",
+	 	              contentType: "application/json",
+	 	              type: 'POST',
+	 	              dataType: "json",
+	 	              data: JSON.stringify({
+	 					"username":"User",
+	 					"message":$scope.myMsg,
+	 					"requestId":$scope.reqid	 					
+	 	                }),
+	 	          }).done(function (data) {
+	 	              console.log("successfully send request form");
+	 	          }); 	 	
 	 }
 
 });
