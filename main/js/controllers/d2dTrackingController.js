f2463e5ef17dc137ae5e5edb72d4e8b78b8f0a59
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2016-03-24 14:31:27 +0800

    updated

--- main/js/controllers/d2dTrackingController.js
+++ main/js/controllers/d2dTrackingController.js
@@ -4,24 +4,97 @@ MetronicApp.controller('d2dTrackingController', function($rootScope, $scope, $ht
     $scope.$on('$viewContentLoaded', function() {   
         // initialize core components
         Metronic.initAjax();
-
-
-
-
-
         $scope.timelineChart = function(){
 
-
-
        // console.log(data);
-
-
 
           var seriesOptions = [],
                seriesCounter = 0,
                names = ['1', '2'];
 
+          var seriesDet = {"series":[
+              {"name":"1", "title":"Exit", "color":"#B7D8F5"}, 
+              {"name":"2", "title":"Entry", "color":"#F5B7BC"}
+          ]};
 
+
+
+          (function (H) {
+              H.wrap(H.Axis.prototype, 'setExtremes', function (proceed) {
+                  var newMin = arguments[1],
+                      newMax = arguments[2];
+                  
+                  arguments[1] = (newMin+newMax)/2;
+                  
+                  // Run original proceed method
+                  proceed.apply(this, [].slice.call(arguments, 1));
+              });
+          }(Highcharts));
+
+          function afterSetExtremes(e) {
+            console.log(e);
+            var chart = $('#container').highcharts();
+
+            chart.showLoading('Loading data from server...');
+
+
+            var dateFormat = function(ele) {
+              var myDate = new Date(ele);
+
+              var yyyy = myDate.getFullYear().toString();
+            
+              var mm = (myDate.getMonth()+1).toString(); // getMonth() is zero-based
+              var dd  = myDate.getDate().toString();
+               // console.log(yyyy+"-" + (mm[1]?mm:"0"+mm[0])+"-" + (dd[1]?dd:"0"+dd[0])+"T00:00:00Z");
+              return yyyy+"-" + (mm[1]?mm:"0"+mm[0])+"-" + (dd[1]?dd:"0"+dd[0])+"T00:00:00Z"; // padding
+            
+             };
+
+
+             $.each(names, function (j, name) {
+
+
+            var query ='q=dy_action_ind:1&rows='+name+'&json.facet={in_outs:{type : range,field : xit_date,start :"'+dateFormat(Math.round(e.min))+'",end :"'+dateFormat(Math.round(e.max))+'",gap:"%2B1DAY"}}'// "q=-mad_crt_dt%3A\"1900-01-01T00%3A00%3A00Z\"&json.facet ={\"min_date\":\"min(mad_crt_dt)\",\"max_date\":\"max(mad_crt_dt)\"}}"
+            var sq = "http://"+solrHost+":8983/solr/his/query?"
+            $http.get(sq+query).
+               success(function(data) {
+                  //chart.series[0].setData(data);
+                  //chart.hideLoading();
+
+                      var storeData = [];
+                  for( var i=0,l = data.facets.in_outs.buckets.length;i<l; i++){
+                    var obj = data.facets.in_outs.buckets[i];
+                    var element =[];
+                    element.push(new Date(obj.val).getTime());
+                    element.push(obj.count);
+                    storeData.push(element);
+                  }
+
+                   console.log(storeData);
+
+
+                  seriesOptions[j] = {
+                      name: name,
+                      data: storeData,
+                       type : 'areaspline',
+                       threshold : null
+                  };
+
+                  chart.hideLoading();
+
+                  // As we're loading the data asynchronously, we don't know what order it will arrive. So
+                  // we keep a counter and create the chart when all the data is loaded.
+                  seriesCounter += 1;
+
+                  if (seriesCounter === names.length) {
+                      $scope.createChart();
+                  }
+               });
+             });
+      
+        }
+
+               
            /**
             * Create the chart when all data is loaded
             * @returns {undefined}
@@ -47,6 +120,20 @@ MetronicApp.controller('d2dTrackingController', function($rootScope, $scope, $ht
                        }]
                    },*/
 
+
+                   chart: {
+                zoomType: 'x'
+
+            },
+
+            xAxis : {
+                events : {
+                    afterSetExtremes : afterSetExtremes
+                },
+                minRange: 3600 * 1000 // one hour
+            },
+
+
                    plotOptions: {
                        
                    },
@@ -60,18 +147,57 @@ MetronicApp.controller('d2dTrackingController', function($rootScope, $scope, $ht
                });
            }
 
-           $.each(names, function (j, name) {
-
-
-
-                var query ='q=dy_action_ind:'+name+'&rows=1&json.facet={in_outs:{type : range,field : xit_date,start : "2015-01-01T00:00:00Z",end :"2016-01-23T00:00:00Z",gap:"%2B1DAY"}}'// "q=-mad_crt_dt%3A\"1900-01-01T00%3A00%3A00Z\"&json.facet ={\"min_date\":\"min(mad_crt_dt)\",\"max_date\":\"max(mad_crt_dt)\"}}"
+           $.each(seriesDet, function(j, valu){
+            $.each(valu, function(m, k){
+               var query ='q=dy_action_ind:'+k.name+'&rows=1&fq=xit_date:[NOW-6MONTH%20TO%20NOW]&json.facet={in_outs:{type : range,field : xit_date,start : "2015-01-01T00:00:00Z",end :"2016-01-23T00:00:00Z",gap:"%2B1DAY"}}'// "q=-mad_crt_dt%3A\"1900-01-01T00%3A00%3A00Z\"&json.facet ={\"min_date\":\"min(mad_crt_dt)\",\"max_date\":\"max(mad_crt_dt)\"}}"
                 var sq = "http://"+solrHost+":8983/solr/his/query?"
                 $http.get(sq+query).
                  success(function(data) {
                   //console.log(data);
                   
                       var storeData = [];
-                   for( var i=0,l = data.facets.in_outs.buckets.length;i<l; i++){
+                  for( var i=0,l = data.facets.in_outs.buckets.length;i<l; i++){
+                    var obj = data.facets.in_outs.buckets[i];
+                    var element =[];
+                    element.push(new Date(obj.val).getTime());
+                    element.push(obj.count);
+                    storeData.push(element);
+                  }
+
+                   console.log(storeData);
+
+
+                  seriesOptions[m] = {
+                      name: k.name,
+                      color: k.color,
+                      data: storeData,
+                      type : 'areaspline',
+                      threshold : null
+                  };
+
+                  // As we're loading the data asynchronously, we don't know what order it will arrive. So
+                  // we keep a counter and create the chart when all the data is loaded.
+                  seriesCounter += 1;
+
+                  if (seriesCounter === names.length) {
+                      $scope.createChart();
+                  }
+
+                 });
+            });
+           });
+
+           /*$.each(names, function (j, name) {
+
+
+                var query ='q=dy_action_ind:'+name+'&rows=1&fq=xit_date:[NOW-6MONTH%20TO%20NOW]&json.facet={in_outs:{type : range,field : xit_date,start : "2015-01-01T00:00:00Z",end :"2016-01-23T00:00:00Z",gap:"%2B1DAY"}}'// "q=-mad_crt_dt%3A\"1900-01-01T00%3A00%3A00Z\"&json.facet ={\"min_date\":\"min(mad_crt_dt)\",\"max_date\":\"max(mad_crt_dt)\"}}"
+                var sq = "http://"+solrHost+":8983/solr/his/query?"
+                $http.get(sq+query).
+                 success(function(data) {
+                  //console.log(data);
+                  
+                      var storeData = [];
+                  for( var i=0,l = data.facets.in_outs.buckets.length;i<l; i++){
                     var obj = data.facets.in_outs.buckets[i];
                     var element =[];
                     element.push(new Date(obj.val).getTime());
@@ -85,8 +211,8 @@ MetronicApp.controller('d2dTrackingController', function($rootScope, $scope, $ht
                   seriesOptions[j] = {
                       name: name,
                       data: storeData,
-                       type : 'areaspline',
-                       threshold : null
+                      type : 'areaspline',
+                      threshold : null
                   };
 
                   // As we're loading the data asynchronously, we don't know what order it will arrive. So
@@ -96,37 +222,13 @@ MetronicApp.controller('d2dTrackingController', function($rootScope, $scope, $ht
                   if (seriesCounter === names.length) {
                       $scope.createChart();
                   }
-                  //$scope.timelineChart(data);
 
-                   //var y = {};
-                   //y.min = $scope.yyyymmdd(new Date(data.facets.min_date));
-                   //y.max = $scope.yyyymmdd(new Date(data.facets.max_date));
-                   //console.log(y);
-                   //$scope.dateRange = y;
                  });
 
                
 
 
-              /* $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
-                //console.log(data);
-
-                   seriesOptions[i] = {
-                       name: name,
-                       data: data,
-                        type: 'area',
-                        gapSize: 5
-                   };
-
-                   // As we're loading the data asynchronously, we don't know what order it will arrive. So
-                   // we keep a counter and create the chart when all the data is loaded.
-                   seriesCounter += 1;
-
-                   if (seriesCounter === names.length) {
-                       $scope.createChart();
-                   }
-               });*/
-           });
+           });*/
          };
 
           $scope.timelineChart();
