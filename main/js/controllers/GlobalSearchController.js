6171ff49967e0ace41817c2e6626446d71689e5f
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2016-06-30 13:37:43 +0800

    search by map has been done

--- main/js/controllers/GlobalSearchController.js
+++ main/js/controllers/GlobalSearchController.js
@@ -62,14 +62,14 @@ MetronicApp.controller('GlobalSearchController', function ($rootScope, $scope, $
   // don't forget to include leaflet-heatmap.js
 
   var greenIcon = L.icon({
-    iconUrl: 'assets/pistachio/map/leaf-green.png',
-    shadowUrl: 'assets/pistachio/map/leaf-shadow.png',
+    //iconUrl: 'assets/pistachio/map/leaf-green.png',
+    //shadowUrl: 'assets/pistachio/map/leaf-shadow.png',
 
     iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    //shadowSize:   [50, 64], // size of the shadow
+    //iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    //shadowAnchor: [4, 62],  // the same for the shadow
+    //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
   $scope.drawHeatMap = function () {
@@ -80,16 +80,15 @@ MetronicApp.controller('GlobalSearchController', function ($rootScope, $scope, $
 
     var clickCircle, clickMarker;
     map.on('singleclick', function (e) {
-      $(".range-slider__range").val('1');
-      $(".range-slider__value").text('1 KM');
+      $(".range-slider__range").val('20');
+      $(".range-slider__value").text('20 KM');
       console.log('singleclick', e);
       $rootScope.triggerFunc = function(km){
-        console.log('hello');
         if ($scope.clickCircle != undefined) {
         map.removeLayer($scope.clickCircle);
         map.removeLayer(clickMarker);
       };
-      clickMarker = L.marker([e.latlng.lat, e.latlng.lng], {icon: greenIcon}).addTo(map);
+      clickMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
       $scope.clickCircle = L.circle([e.latlng.lat, e.latlng.lng], km, {
         color: 'red',
         fillColor: '#f03',
@@ -100,7 +99,7 @@ MetronicApp.controller('GlobalSearchController', function ($rootScope, $scope, $
         .openOn(map);
       };
       
-      $scope.triggerFunc(1000);
+      $scope.triggerFunc(20000);
     });
 
     map.on('load', function (e) {
