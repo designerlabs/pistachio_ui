71cb14279eb667552327f492f9d2fc4d88084d0d
Adline Dsilva <adline.dsilva@mimos.my>
2016-08-04 12:47:59 +0800

    app:L

--- main/js/app.js
+++ main/js/app.js
@@ -265,7 +265,7 @@ angular.module('myModule').config(['$controllerProvider', function($controllerPr
 /* Global hosting details */
 
 //var globalURL = "http://10.23.124.243:8080/";
-var globalURL = "http://10.1.17.25:8080/";
+var globalURL = "http://10.1.17.25:8081/";
 //var globalURL = "http://pistachio_server:8080/";
 //var solrHost = "10.23.124.220";
 var solrHost = "10.4.104.176";
@@ -1086,6 +1086,10 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                     insertBefore: '#ng_load_plugins_before', // load the above css files before a LINK element with this ID. Dynamic CSS files must be loaded between core and theme css files
                     files: [
                         'assets/pistachio/stats/stats.css',
+                        'assets/global/plugins/highcharts/js/highcharts.js',
+                        'assets/global/plugins/bootstrap-daterangepicker/daterangepicker.css',
+                        'assets/global/plugins/bootstrap-daterangepicker/daterangepicker.js',
+                        'assets/global/plugins/bootstrap-daterangepicker/moment.js',
                         'js/controllers/StatsChartController.js'
                     ]
                 });
